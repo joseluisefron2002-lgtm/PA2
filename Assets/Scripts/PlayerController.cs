aed0c86765764880b6ac1bf4b71101682e8ecef3
joseluisefron2002-lgtm <joseluisefron2002@gamil.com>
2026-09-12 10:50:02 -0500

    Inicio

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody2D rb2D;
+
+    private float move;
+    public float jumpforce = 4;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float groundRadius = 0.1f;
+    public LayerMask groundLayer;
+
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+       move = Input.GetAxisRaw("Horizontal");
+       rb2D.linearVelocity = new Vector2(move*speed, rb2D.linearVelocity.y);
+
+       if (move != 0){
+           transform.localScale = new Vector3(Mathf.Sign(move), 1, 1);
+       }
+
+       if (Input.GetButtonDown("Jump") && isGrounded){
+           rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpforce);
+       }
+
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+    }
+
+
+
+}
